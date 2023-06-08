f20f48ec639b994a0b8507141f693916bfc3e3b3
Clownmaidlifestyle <e.edson@protonmail.ch>
2023-06-08 14:25:51 +0100

    nearly working toggleable dark mode button

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
+function initialise(){
+    console.log("fuck me tbh");
+}
+
 function darkMode() {
     let darkModeToggle = document.body;
     darkModeToggle.classList.toggle("dark-mode");
+    document.getElementById("buttonimage").src="images/button-night.png";
 }
+
