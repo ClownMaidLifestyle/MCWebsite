bba466973b6528a93bfd07a652563adaf2ccf69e
Clownmaidlifestyle <e.edson@protonmail.ch>
2023-06-08 14:37:56 +0100

    FUNCTIONAL DARK MODEgit add .git add .

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
-function initialise(){
-    console.log("fuck me tbh");
+let isDay
+
+function initialise() {
+    isDay = true;
 }
 
 function darkMode() {
     let darkModeToggle = document.body;
     darkModeToggle.classList.toggle("dark-mode");
-    document.getElementById("buttonimage").src="images/button-night.png";
+
+    if (isDay == true){
+        document.getElementById("buttonimage").src="images/button-night.png";
+        isDay = false;
+    }
+    else{
+        document.getElementById("buttonimage").src="images/button-day.png";
+        isDay = true;
+    }
+    
 }
 
